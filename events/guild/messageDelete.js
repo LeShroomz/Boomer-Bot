b98a8aee42cbcfa712be34f84d712ddc0bd1240c
NotTrixxie <patu.tuomineeen@gmail.com>
2022-04-25 22:29:31 +0300

    Fixed messageDelete error
    Embed messages sometimes are null content

--- events/guild/messageDelete.js
+++ events/guild/messageDelete.js
@@ -5,16 +5,25 @@ const IC = require("../../botconfig/internalChannels.json");
 module.exports = async (client, message) => {
 
     if(message.author && message.author.bot) return;
-    let oldContent = truncateString(message.content, 1020);
-    let embed = new MessageEmbed()
-    .setAuthor({name: `MESSAGE DELETED`})
-    .setDescription(`Deleted message in ${message.channel}`)
-    .addFields(
-        {name: `MESSAGE`, value: `${oldContent}`},
-    )
-    .setTimestamp()
-    .setColor(`DARK_RED`)
-    .setFooter({text: `${message.author.tag}`, iconURL: message.member.displayAvatarURL()})
+    if(message.content == null){
+        if(message.embeds[0] != null){
+            let embed = message.embeds[0];
+            embed.setAuthor({name: `MESSAGE DELETED`}).setDescription(`Deleted message in ${message.channel}`)
 
-    await client.channels.cache.get(IC.botcommands).send({embeds: [embed]});
+            await client.channels.cache.get(IC.botcommands).send({embeds: [embed]});
+        } else return;
+    } else {
+        let oldContent = truncateString(message.content, 1020);
+        let embed = new MessageEmbed()
+        .setAuthor({name: `MESSAGE DELETED`})
+        .setDescription(`Deleted message in ${message.channel}`)
+        .addFields(
+            {name: `MESSAGE`, value: `${oldContent}`},
+        )
+        .setTimestamp()
+        .setColor(`DARK_RED`)
+        .setFooter({text: `${message.author.tag}`, iconURL: message.member.displayAvatarURL()})
+
+        await client.channels.cache.get(IC.botcommands).send({embeds: [embed]});
+    }
 }
