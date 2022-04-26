c86b253d656f2443b0ba2b4efdaf33943cfba7f9
Shhroomz <patu.tuomineeen@gmail.com>
2022-04-26 23:45:48 +0300

    Log fixes

--- events/guild/messageDelete.js
+++ events/guild/messageDelete.js
@@ -5,16 +5,39 @@ const IC = require("../../botconfig/internalChannels.json");
 module.exports = async (client, message) => {
 
     if(message.author && message.author.bot) return;
-    let oldContent = truncateString(message.content, 1020);
-    let embed = new MessageEmbed()
-    .setAuthor({name: `MESSAGE DELETED`})
-    .setDescription(`Deleted message in ${message.channel}`)
-    .addFields(
-        {name: `MESSAGE`, value: `${oldContent}`},
-    )
-    .setTimestamp()
-    .setColor(`DARK_RED`)
-    .setFooter({text: `${message.author.tag}`, iconURL: message.member.displayAvatarURL()})
 
-    await client.channels.cache.get(IC.botcommands).send({embeds: [embed]});
+    if(message.attachments.size > 0){
+        if (message.attachments.every(attachIsImage)){
+            let embedimg = new MessageEmbed()
+            .setAuthor({name: `IMAGE DELETED`})
+            .setDescription(`Image sent by ${message.author.tag} in ${message.channel} was deleted`)
+            .setImage(message.attachments.first().proxyURL)
+            .setTimestamp()
+            .setColor(`DARK_RED`)
+            .setFooter({text: `Author ID: ${message.author.id}`, iconURL: message.member.displayAvatarURL()})
+
+            client.channels.cache.get(IC.botcommands).send({embeds: [embedimg]});
+        }
+    }
+
+    if(message.content != null && message.content.length > 0){
+        let oldContent = truncateString(message.content, 1020);
+        let embed = new MessageEmbed()
+        .setAuthor({name: `MESSAGE DELETED`})
+        .setDescription(`Message sent by ${message.author.tag} in ${message.channel} was deleted`)
+        .addFields(
+            {name: `MESSAGE`, value: `${oldContent}`},
+        )
+        .setTimestamp()
+        .setColor(`DARK_RED`)
+        .setFooter({text: `Author ID: ${message.author.id}`, iconURL: message.member.displayAvatarURL()})
+
+        client.channels.cache.get(IC.botcommands).send({embeds: [embed]});
+    }
+}
+
+function attachIsImage(msgAttach) {
+    var url = msgAttach.url;
+    //True if this url is a png image.
+    return url.indexOf("png", url.length - "png".length /*or 3*/) !== -1;
 }
